377ba324c0040594ba6be12bd76a585e2e007c96
Brady Diamond <brdydmnd@gmail.com>
2022-04-08 13:31:39 -0700

    console debbugging

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,11 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
     const size = $("#size").val() + " ";
-    const toppings= " " + $("#topping").val();
-    // let toppings = [];
-    // $("toppings").each(function(toppings) {
-    //   toppings.push($('#topping').val());
-    // });
+    // const toppings= " " + $("#topping").val();
+    let toppings = [];
+    $("topping").each(function(element) {
+      topping.push($('#topping').val());
+    });
     let myPizza = new Pizza(size, toppings)
     console.log(myPizza)
     const outputArray = Object.values(myPizza)
@@ -31,3 +31,5 @@ $(document).ready(function() {
    
   });
 });
+
+
