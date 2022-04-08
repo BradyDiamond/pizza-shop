3b4f0d55d22dec99495363d5cbb7c429d914c990
Brady Diamond <brdydmnd@gmail.com>
2022-04-08 10:12:27 -0700

    console debbugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,44 @@
-//Buisness Logic
-function BuildPizza () { 
-  this.pizza 
-}
 
 
-function Pizza(size, cheese, sauce, toppings) {
+function Pizza(size, toppings) {
 this.size = size;
-this.cheese = cheese;
-this.sauce = sauce;
 this.toppings = toppings;
+}
+Pizza.prototype.addTopping = function(topping) {
+ this.Pizza.push(topping);
+}
 
-Pizza.prototype.buildPizza = function() {
- return this.size + ' ' + this.cheese + ' ' + this.sauce + ' ' + this.toppings
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // UI logic
 
 
-$(document).ready(function() {
-  $("form#name").submit(function(event) {
-    event.preventDefault();
-    const flavor = $("input:radio[name=size]:checked").val();
-    const firstName = $("#name1").val();
-    const lastName = $("#name2").val();
-    const result = (firstName + " " + lastName);
-    const receiptText = " Thank you for your Purchase!"
-    $("#output").text(result + " - " + flavor.toUpperCase() + " " + receiptText);
-    $("#output").show();
-  });
-});
+// $(document).ready(function() {
+//   $("form#order").submit(function(event) {
+//     event.preventDefault();
+//     const size = parseInt($("#size").val());
+//     $("#output").show();
+//   });
+// });
