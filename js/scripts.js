e7f02d193422e4d5d5cdae70ea0a0d94429bdc7d
Brady Diamond <brdydmnd@gmail.com>
2022-04-08 09:19:54 -0700

    add initial form and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 //Buisness Logic
-
+function BuildPizza () { 
+  this.pizza 
+}
 
 
 function Pizza(size, cheese, sauce, toppings) {
 this.size = size;
-// this.cheese = cheese;
-// this.sauce = sauce;
-this.toppings = toppings
+this.cheese = cheese;
+this.sauce = sauce;
+this.toppings = toppings;
 
-Pizza.prototype.builtPizza = function() {
- return 
+Pizza.prototype.buildPizza = function() {
+ return this.size + ' ' + this.cheese + ' ' + this.sauce + ' ' + this.toppings
 }
 }
+
+
+// UI logic
+
+
+$(document).ready(function() {
+  $("form#name").submit(function(event) {
+    event.preventDefault();
+    const flavor = $("input:radio[name=size]:checked").val();
+    const firstName = $("#name1").val();
+    const lastName = $("#name2").val();
+    const result = (firstName + " " + lastName);
+    const receiptText = " Thank you for your Purchase!"
+    $("#output").text(result + " - " + flavor.toUpperCase() + " " + receiptText);
+    $("#output").show();
+  });
+});
